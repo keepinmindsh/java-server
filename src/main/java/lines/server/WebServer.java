ecf439217ef60504d9f228642ce5c9d1baad027f
lines_makers <keepinmindsh@gmail.com>
2021-08-22 21:14:51 +0900

    java-server project start!

---
+++ src/main/java/lines/server/WebServer.java
@@ -0,0 +1,30 @@
+package lines.server;
+
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class WebServer {
+
+    private static final int DEFAULT_PORT = 8080;
+
+    public static void main(String[] args) throws Exception {
+        int port = 0;
+
+        if ( args == null || args.length == 0){
+            port = DEFAULT_PORT;
+        }else {
+            port = Integer.parseInt(args[0]);
+        }
+
+        // 서버 소켓을 생성한다. 웹 서버는 기본적으로 8080번 포트를 생성한다.
+        try(ServerSocket listenSocket = new ServerSocket(port)){
+
+            // 클라이언트가 연결할 때 까지 대기한다.
+            Socket connection;
+
+            while((connection = listenSocket.accept()) != null){
+
+            }
+        }
+    }
+}
